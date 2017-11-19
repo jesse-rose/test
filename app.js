dfd5a085b305c1f45c04c0b8c0529f38ea53353f
jesse-rose <jesse.rose.2268@gmail.com>
2017-11-19 15:07:19 -0500

    bla

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log("hello Jesse !!!");
+
+let i = 0;
+
+let things = [1,2,3,4,5,4,5,6,7];
+
+let r1 = things.find(v => v > 3);
+
+console.log(r1);
+
+let r2 = things.filter(v => v > 4);
+
+console.log(`results: ${r2}`);
+
+
